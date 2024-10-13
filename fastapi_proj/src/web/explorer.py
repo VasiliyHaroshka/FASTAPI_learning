3fbbf6eb7af21947782dc1d3fdc6eb77f01128bd
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-13 17:04:12 +0300

    add CRUD for explores in web

--- fastapi_proj/src/web/explorer.py
+++ fastapi_proj/src/web/explorer.py
@@ -1,8 +1,36 @@
 from fastapi import APIRouter
 
+from ..fake import explorer as service
+from ..model.explorer import Explorer
+
 router = APIRouter(prefix="/explorer")
 
 
 @router.get("/")
-def start_explorer():
-    return "start explorer"
+def get_all() -> list[Explorer]:
+    return service.get_all_explores()
+
+
+@router.get("/{name}")
+def get_one(name: str) -> Explorer:
+    return service.get_one_explorer(name)
+
+
+@router.post("/")
+def create(explorer: Explorer) -> Explorer:
+    return service.create_explorer(explorer)
+
+
+@router.patch("/")
+def create(explorer: Explorer) -> Explorer:
+    return service.update_explorer(explorer)
+
+
+@router.put("/")
+def create(explorer: Explorer) -> Explorer:
+    return service.replace_explorer(explorer)
+
+
+@router.delete("/{name}")
+def create(name: str) -> Explorer:
+    return service.delete_explorer(name)
