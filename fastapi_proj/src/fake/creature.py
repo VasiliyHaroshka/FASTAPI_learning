85fce599a41edd0e06e05834412e626fa068489f
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-13 17:07:52 +0300

    add CRUD for creatures in web

--- fastapi_proj/src/fake/creature.py
+++ fastapi_proj/src/fake/creature.py
@@ -18,30 +18,30 @@ fake_creatures = [
 ]
 
 
-def get_all_explores() -> list[Creature]:
+def get_all_creatures() -> list[Creature]:
     return fake_creatures
 
 
-def get_one_explorer(name: str) -> Creature | None:
+def get_one_creature(name: str) -> Creature | None:
     for creature in fake_creatures:
         if creature.name == name:
             return creature
     return None
 
 
-def create_explorer(creature: Creature) -> Creature:
+def create_creature(creature: Creature) -> Creature:
     return creature
 
 
-def update_explorer(creature: Creature) -> Creature:
+def update_creature(creature: Creature) -> Creature:
     return creature
 
 
-def replace_explorer(creature: Creature) -> Creature:
+def replace_creature(creature: Creature) -> Creature:
     return creature
 
 
-def delete_explorer(name: str) -> Creature | None:
+def delete_creature(name: str) -> Creature | None:
     for creature in fake_creatures:
         if creature.name == name:
             fake_creatures.remove(creature)
---
+++ fastapi_proj/src/web/creature.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+
+from ..fake import creature as service
+from ..model.creature import Creature
+
+router = APIRouter(prefix="/creature")
+
+
+@router.get("/")
+def get_all() -> list[Creature]:
+    return service.get_all_creatures()
+
+
+@router.get("/{name}")
+def get_one(name: str) -> Creature:
+    return service.get_one_creature(name)
+
+
+@router.post("/")
+def create(creature: Creature) -> Creature:
+    return service.create_creature(creature)
+
+
+@router.patch("/")
+def create(creature: Creature) -> Creature:
+    return service.update_creature(creature)
+
+
+@router.put("/")
+def create(creature: Creature) -> Creature:
+    return service.replace_creature(creature)
+
+
+@router.delete("/{name}")
+def create(name: str) -> Creature:
+    return service.delete_creature(name)
