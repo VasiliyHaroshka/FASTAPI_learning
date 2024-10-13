51d042f522fdceab9f566bebebca3e2ca6c50bbf
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-13 16:35:03 +0300

    add list of fake explorers

---
+++ fastapi_proj/src/fake/explorer.py
@@ -0,0 +1,17 @@
+from ..model.explorer import Explorer
+
+fake_explores = [
+    Explorer(
+        name="Bob",
+        country="GB",
+        description="bla-bla-bla",
+    ),
+    Explorer(
+        name="Tom",
+        country="USA",
+        description="bla-bla-bla",
+    ),
+]
+
+
+
