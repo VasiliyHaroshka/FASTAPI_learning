3eb849f351857f9c02009c53389f08fb35ccb969
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-21 20:37:05 +0300

    add positive and negative for creature.create

--- fastapi_proj/src/test/unit/web/test_creature.py
+++ fastapi_proj/src/test/unit/web/test_creature.py
@@ -1,9 +1,11 @@
 import pytest
 import os
 
+from error import Duplicate
 from model.creature import Creature
 
 os.environ["CREATURE_SQLITE_DB"] = ":memory:"
+from data import creature
 
 
 @pytest.fixture
@@ -15,3 +17,13 @@ def sample() -> Creature:
         description="Blood sucker",
         aka="Nosferatu",
     )
+
+
+def test_create(sample):
+    result = creature.create(sample)
+    assert sample == result
+
+
+def test_create_duclicate(sample):
+   with pytest.raises(Duplicate):
+       _ = creature.create(sample)
