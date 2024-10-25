20b8fbe602db6c39a70badf0b4144defea10a672
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-25 19:02:47 +0300

    add get_one to data/user

--- fastapi_proj/src/data/user.py
+++ fastapi_proj/src/data/user.py
@@ -1,3 +1,5 @@
+from ecdsa.test_pyecdsa import params
+
 from model.user import User
 from error import Duplicate, Missing
 from .init import curs, get_db, IntegrityError
@@ -27,3 +29,15 @@ def row_to_model(row: tuple) -> User:
 
 def model_to_dict(user: User) -> dict:
     return user.model_dump()
+
+
+def get_one(name: str) -> User:
+    if not name:
+        return False
+    query = "SELECT * FROM User WHERE name=:name"
+    params = {"name": name}
+    curs.execute(query, params)
+    row = curs.fetchone()
+    if row:
+        return row_to_model(row)
+    raise Missing(f"User with username = {name} is not found")
