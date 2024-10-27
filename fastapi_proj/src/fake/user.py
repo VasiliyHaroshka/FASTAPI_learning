b05bca0d9b28b3e089a469ec9ff9110782b98442
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-27 15:16:44 +0300

    add find and check_missing to fake.user

--- fastapi_proj/src/fake/user.py
+++ fastapi_proj/src/fake/user.py
@@ -1,3 +1,4 @@
+from error import Missing
 from model.user import User
 
 fake_users = [
@@ -12,4 +13,13 @@ fake_users = [
 ]
 
 
+def find(name: str) -> User | None:
+    for user in fake_users:
+        if user.name == name:
+            return user
+    return None
 
+
+def check_missing(name: str):
+    if not find(name):
+        raise Missing(msg=f"User with name = {name} is not found")
