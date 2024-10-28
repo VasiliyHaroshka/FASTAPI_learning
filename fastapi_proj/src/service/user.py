dfb0a1ccbee72a8b6301666dcf29b42e9a7032a2
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-28 17:52:11 +0300

    add get_hash and get_jwt_username in service.user

--- fastapi_proj/src/service/user.py
+++ fastapi_proj/src/service/user.py
@@ -1,6 +1,7 @@
 import os
-from pydoc import plain
 
+from jose import jwt
+from jose.exceptions import JWTError
 from passlib.context import CryptContext
 
 if os.getenv("CREATURE_UNIT_TEST"):
@@ -15,3 +16,18 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain: str, hash: str) -> bool:
     return pwd_context.verify(plain, hash)
+
+
+def get_hash(plain: str) -> str:
+    return pwd_context.hash(plain)
+
+
+def get_jwt_username(token: str) -> str | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if not username:
+            return None
+    except JWTError:
+        return None
+    return username
