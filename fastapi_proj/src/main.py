d81c9c2f915663789925d5353d3217d5f96ee554
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-30 21:33:48 +0300

    connect user router to main

--- fastapi_proj/src/main.py
+++ fastapi_proj/src/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from web import explorer, creature
+from web import explorer, creature, user
 
 app = FastAPI()
 
 app.include_router(explorer.router)
 app.include_router(creature.router)
+app.include_router(user.router)
 
 
 @app.get("/")
 def start():
-    return "start"
+    return "start app"
 
 
 if __name__ == '__main__':
