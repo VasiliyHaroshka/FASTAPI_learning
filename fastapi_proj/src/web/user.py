b0559f59bd4ac71627642e8cc54a2d3ff945ea7f
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-30 21:09:51 +0300

    add def unauthorised to web/user

--- fastapi_proj/src/web/user.py
+++ fastapi_proj/src/web/user.py
@@ -1,10 +1,24 @@
 import os
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
 if os.getenv("CREATURE_UNIT_TEST"):
     from fake.user import fake_users as service
 else:
     from service import user as service
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 router = APIRouter(prefix="/user")
+
+# this dependency return assess token
+oauth2_dependency = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def unauthorised():
+    raise HTTPException(
+        status_code=401,
+        detail="Error! Wrong username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
