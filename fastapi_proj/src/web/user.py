7236127ef02f1c72bccc3414768d01e41d0ea6a4
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-30 21:31:53 +0300

    add CRUD routers to web/user

--- fastapi_proj/src/web/user.py
+++ fastapi_proj/src/web/user.py
@@ -5,6 +5,9 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
+from error import Missing, Duplicate
+from model.user import User
+
 if os.getenv("CREATURE_UNIT_TEST"):
     from fake.user import fake_users as service
 else:
@@ -46,3 +49,40 @@ async def create_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
 def get_access_token(token: str = Depends(oauth2_dependency)) -> dict:
     """Return current access token"""
     return {"token": token}
+
+
+@router.get("/")
+def get_all() -> list[User]:
+    return service.get_all()
+
+
+@router.get("/{name}")
+def get_one(name: str) -> User:
+    try:
+        return service.get_one(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
+
+
+@router.post("/", status_code=201)
+def create(user: User) -> User:
+    try:
+        return service.create(user)
+    except Duplicate as e:
+        raise HTTPException(status_code=409, detail=e.msg)
+
+
+@router.patch("/{name}")
+def modify(name: str, user: User) -> User:
+    try:
+        return service.modify(name, user)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
+
+
+@router.delete("/{name}")
+def delete(name: str) -> None:
+    try:
+        service.delete(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
