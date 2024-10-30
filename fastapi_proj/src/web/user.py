bfcec77d4acc6b8f4ef56c6ac4e4989c4a22ceb2
VasiliyHaroshka <horvasiliy@mail.ru>
2024-10-30 21:19:26 +0300

    add create_access_token to web/user

--- fastapi_proj/src/web/user.py
+++ fastapi_proj/src/web/user.py
@@ -1,7 +1,9 @@
 import os
+from datetime import timedelta
 
 from fastapi import APIRouter, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 if os.getenv("CREATURE_UNIT_TEST"):
     from fake.user import fake_users as service
@@ -12,7 +14,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter(prefix="/user")
 
-# this dependency return assess token
+# this dependency return access token
 oauth2_dependency = OAuth2PasswordBearer(tokenUrl="token")
 
 
@@ -22,3 +24,18 @@ def unauthorised():
         detail="Error! Wrong username or password",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
+@router.post("/token")
+async def create_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = service.auth_user(
+        form_data.username,
+        form_data.password,
+    )
+    if not user:
+        unauthorised()
+    expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = service.create_access_token(
+        data={"sub": user.username},
+        expires=expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
