57aca03bbbf34c3b95c8d27e2f89d9af65c4010f
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-01 19:01:05 +0300

    create create test in data/user

--- fastapi_proj/src/test/unit/data/test_user.py
+++ fastapi_proj/src/test/unit/data/test_user.py
@@ -8,8 +8,13 @@ from data import user
 
 
 @pytest.fixture
-def sample():
+def sample() -> User:
     return User(
         name="Max",
         hash="secret",
     )
+
+
+def test_create(sample):
+    result = user.create(sample)
+    assert sample == result
