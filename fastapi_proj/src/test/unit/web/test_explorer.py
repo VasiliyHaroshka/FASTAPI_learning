b68bfbdc092f7b654a39979c86111f1c3c35fe14
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-03 14:45:24 +0300

    create unit test for web explorer

---
+++ fastapi_proj/src/test/unit/web/test_explorer.py
@@ -0,0 +1,17 @@
+import os
+
+import pytest
+from fastapi import HTTPException
+
+from model.explorer import Explorer
+from web import explorer
+
+os.environ["CREATURE_UNIT_TEST"] = "true"
+
+
+@pytest.fixture
+def sample():
+    return Explorer(name="Ron",
+                    country="ND",
+                    description="Bro",
+                    )
