124681cfaa97271f9dc359faa95210150cc9acb5
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-06 15:10:25 +0300

    create fixture for unit test creature on service level

--- fastapi_proj/src/test/unit/service/test_creature.py
+++ fastapi_proj/src/test/unit/service/test_creature.py
@@ -1,24 +1,19 @@
+import os
+
+import pytest
+
+os.environ["CREATURE_UNIT_TEST"] = "true"
+
 from model.creature import Creature
-from service import creature as from_my_code
+from data import creature as data
 
 
-sample = Creature(
-    name="Vampire",
-    country="ROM",
-    area="Castle",
-    description="bla-bla-bla",
-    aka="blood",
-)
-
-
-def test_create():
-    result = from_my_code.create(sample)
-    assert result == sample
-
-def test_get_and_exist():
-    result = from_my_code.get_one(sample.name)
-    assert result == sample
-
-def test_not_found():
-    result = from_my_code.get_one("boris")
-    assert result is None
+@pytest.fixture
+def sample():
+    return Creature(
+        name="Vampire",
+        country="ROM",
+        area="Castle",
+        description="bla-bla-bla",
+        aka="blood",
+    )
