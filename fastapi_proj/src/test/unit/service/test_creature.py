ebafd5a355f654a7189a30febd159d9b9709b181
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-06 15:20:07 +0300

    add test create of creature on service level

--- fastapi_proj/src/test/unit/service/test_creature.py
+++ fastapi_proj/src/test/unit/service/test_creature.py
@@ -2,11 +2,12 @@ import os
 
 import pytest
 
+from error import Duplicate
+
 os.environ["CREATURE_UNIT_TEST"] = "true"
 
 from model.creature import Creature
 from data import creature as data
-
 
 @pytest.fixture
 def sample():
@@ -17,3 +18,16 @@ def sample():
         description="bla-bla-bla",
         aka="blood",
     )
+
+
+def test_create(sample: Creature):
+    assert data.create(sample) == sample
+
+def test_create_duplicate(sample: Creature):
+    result = data.create(sample)
+    assert result == sample
+    with pytest.raises(Duplicate):
+        result = data.create(sample)
+
+
+
