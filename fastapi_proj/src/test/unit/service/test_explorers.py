78595a0b366546670150497ca31ecdcf3c3413bd
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-08 19:23:47 +0300

    add test create def in unit test explorer on service level

--- fastapi_proj/src/test/unit/service/test_explorers.py
+++ fastapi_proj/src/test/unit/service/test_explorers.py
@@ -4,6 +4,8 @@ import pytest
 
 from fastapi import HTTPException
 
+from test.unit.web.test_explorer import assert_duplicates
+
 os.environ["CREATURE_UNIT_TEST"] = "true"
 
 from model.explorer import Explorer
@@ -32,3 +34,13 @@ def assert_duplicate(exc):
 def assert_missing(exc):
     assert exc.value.status_code == 404
     assert "is not found" in exc.value.msg
+
+
+def test_create(sample):
+    assert explorer.create(sample) == sample
+
+
+def test_create_duplicate(fakes):
+    with pytest.raises(HTTPException) as exc:
+        _ = explorer.create(fakes[0])
+        assert_duplicates(exc)
