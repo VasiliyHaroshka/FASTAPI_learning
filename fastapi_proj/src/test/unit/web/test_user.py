cc8eb1cf51a58bad3f70ee90604cc888e56cd48e
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-14 19:47:26 +0300

    add changes in imports in test_user unit test on web level

--- fastapi_proj/src/test/unit/web/test_user.py
+++ fastapi_proj/src/test/unit/web/test_user.py
@@ -1,12 +1,10 @@
 import os
 
 import pytest
-
 from fastapi import HTTPException
 
 os.environ["CREATURE_UNIT_TEST"] = "true"
 
-from error import Duplicate, Missing
 from model.user import User
 from web import user
 
