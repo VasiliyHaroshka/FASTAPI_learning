8267fcbe1a8320d281034128d993c6b9574cfa32
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-15 16:35:23 +0300

    add fixtures in file test_user in unit test on service level

--- fastapi_proj/src/test/unit/service/test_user.py
+++ fastapi_proj/src/test/unit/service/test_user.py
@@ -6,3 +6,16 @@ from fastapi import HTTPException
 os.environ["CREATURE_UNIT_TEST"] = "true"
 from model.user import User
 from service import user
+
+
+@pytest.fixture
+def sample() -> User:
+    return User(
+        name="Shon",
+        hash="321",
+    )
+
+
+@pytest.fixture
+def fakes() -> list[User]:
+    return user.get_all()
