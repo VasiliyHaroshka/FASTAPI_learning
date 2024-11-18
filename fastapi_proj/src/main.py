92c043c3bec8117789436a80cc65fcc753f523d1
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-18 19:47:33 +0300

    add file_sizer2 to main

--- fastapi_proj/src/main.py
+++ fastapi_proj/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 
 from web import explorer, creature, user
 
@@ -12,3 +12,7 @@ app.include_router(user.router)
 @app.post("/file_sizer")
 async def upload_file1(file: bytes = File()) -> str:
     return f"This file's size is {len(file)} b"
+
+@app.post("/file_sizer2")
+async def upload_file2(file: UploadFile) -> str:
+    return f"This file's name is {file.filename}. Its size is {file.size} b"
