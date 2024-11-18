67bbe4767f72b0c8a74b7e90f77dadd6812b2284
VasiliyHaroshka <horvasiliy@mail.ru>
2024-11-18 20:01:59 +0300

    add file generator

--- fastapi_proj/src/main.py
+++ fastapi_proj/src/main.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+from typing import Generator
+
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 
 from web import explorer, creature, user
 
@@ -14,10 +17,17 @@ app.include_router(user.router)
 async def upload_file1(file: bytes = File()) -> str:
     return f"This file's size is {len(file)} b"
 
+
 @app.post("/file_sizer2")
 async def upload_file2(file: UploadFile) -> str:
     return f"This file's name is {file.filename}. Its size is {file.size} b"
 
+
 @app.get("/download/{name}")
 async def download(name):
     return FileResponse(name)
+
+
+def file_generator(path: str) -> Generator:
+    with open(file=path, mode="rb") as file:
+        yield file.read()
