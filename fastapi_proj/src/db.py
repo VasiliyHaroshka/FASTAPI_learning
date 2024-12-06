de9a4adb776f8ded949bf3c3ae6dcb96feb92032
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-06 18:59:57 +0300

    add get session

--- fastapi_proj/src/db.py
+++ fastapi_proj/src/db.py
@@ -2,7 +2,13 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 engine = create_async_engine("sqlite+aiosqlite:///database.db")
 
-session = async_sessionmaker(
+new_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
 )
+
+
+async def get_session():
+    """Session generator"""
+    async with new_session() as session:
+        yield session
