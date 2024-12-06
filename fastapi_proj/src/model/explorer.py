f2254df9a42eda3e9abc14c65423f249a5e8d403
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-06 19:16:19 +0300

    add constrains to explorer model

--- fastapi_proj/src/model/explorer.py
+++ fastapi_proj/src/model/explorer.py
@@ -6,7 +6,7 @@ from base import Base
 class Explorer(Base):
     __tablename__ = "explorer"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column()
-    country: Mapped[str] = mapped_column()
-    description: Mapped[str] = mapped_column()
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    country: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
