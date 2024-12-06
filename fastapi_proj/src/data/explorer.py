f2581437316ef36a508cdf726fcc49b0d2394597
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-06 20:00:59 +0300

    change create explorer def

--- fastapi_proj/src/data/explorer.py
+++ fastapi_proj/src/data/explorer.py
@@ -1,6 +1,15 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from data.init import curs, IntegrityError
+from db import get_session
 from error import Duplicate, Missing
 from model.explorer import Explorer
+from schemas.explorer import ExplorerAddSchema
+
+Session_dep = Annotated[AsyncSession, Depends(get_session)]
 
 curs.execute("""
     CREATE TABLE IF NOT EXISTS Explorer(
@@ -39,17 +48,28 @@ def get_all() -> list[Explorer]:
     return [row_to_model(row) for row in curs.fetchall()]
 
 
-def create(explorer: Explorer) -> Explorer:
-    if not explorer:
-        return None
-    query = """INSERT INTO Explorer (name, country, description)
-                VALUES (:name, :country, :description)"""
-    params = model_to_dict(explorer)
-    try:
-        curs.execute(query, params)
-    except IntegrityError:
-        raise Duplicate(msg=f"Explorer {explorer.name} is already exists in db")
-    return get_one(explorer.name)
+# def create(explorer: Explorer) -> Explorer:
+#     if not explorer:
+#         return None
+#     query = """INSERT INTO Explorer (name, country, description)
+#                 VALUES (:name, :country, :description)"""
+#     params = model_to_dict(explorer)
+#     try:
+#         curs.execute(query, params)
+#     except IntegrityError:
+#         raise Duplicate(msg=f"Explorer {explorer.name} is already exists in db")
+#     return get_one(explorer.name)
+
+
+async def create(data: ExplorerAddSchema, session: Session_dep):
+    new_explorer = Explorer(
+        name=data.name,
+        country=data.country,
+        description=data.description,
+    )
+    session.add(new_explorer)
+    await session.commit()
+    return get_one(new_explorer.name)
 
 
 def modify(name: str, explorer: Explorer) -> Explorer:
