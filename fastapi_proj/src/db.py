861f8c79b9a7f04f39a08ef688230a859bc8ad5b
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-06 18:57:21 +0300

    add session factory

--- fastapi_proj/src/db.py
+++ fastapi_proj/src/db.py
@@ -1,3 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 engine = create_async_engine("sqlite+aiosqlite:///database.db")
+
+session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
