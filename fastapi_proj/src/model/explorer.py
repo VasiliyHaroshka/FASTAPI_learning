f426eb06f7151c70642344769d798d8e3cea4069
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-06 19:09:37 +0300

    change model explorer

--- fastapi_proj/src/model/explorer.py
+++ fastapi_proj/src/model/explorer.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+
+from base import Base
 
 
-class Explorer(BaseModel):
-    name: str
-    country: str
-    description: str
+class Explorer(Base):
+    __tablename__ = "explorer"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    country: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
